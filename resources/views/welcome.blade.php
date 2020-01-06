195e0d3bdf24852763d8a662a65a0071194cb765
Franco <zekropls@gmail.com>
2020-01-05 23:44:57 -0300

    aa r: proyecto test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel1</title>
+        <title>LaraveXapalo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
